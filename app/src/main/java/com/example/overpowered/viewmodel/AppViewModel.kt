25896f06fa66713fdceefb2259a45b3e45795a4e
InViSiB0B <Ceaton157@gmail.com>
2025-11-10 19:49:15 -0500

    Merged from charlesDev:
    New UI look and theming
    
    Code cleanup
    
    Updated with a few fixed to TopStatusBar and MainAppScaffold

--- app/src/main/java/com/example/overpowered/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/example/overpowered/viewmodel/AppViewModel.kt
@@ -656,7 +656,7 @@ class AppViewModel : ViewModel() {
             _isLoadingLeaderboard.value = true
 
             try {
-                // Get friend IDs
+                // Get friend IDss
                 val friendIds = _friends.value.map { it.friendId }
 
                 if (friendIds.isEmpty()) {
